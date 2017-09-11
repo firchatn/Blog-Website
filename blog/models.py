4be8cc68e6cb36cfef15390f783d34f75f575e7a
Firas Chaaben <firaschaabencss@gmail.com>
2017-09-11 15:16:43 +0100

    project blog init

---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+
+
+
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    email =  models.EmailField()
+    phone = models.CharField(max_length=20)
+    img = models.ImageField(upload_to='upload/')
+    joind_date = models.DateTimeField(
+        'date published', auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.id
+
+
+class Article(models.Model):
+	autor = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=20)
+	aboutnow = models.CharField(max_length=3000)
+	img = models.ImageField(upload_to='upload/')
+	post_date = models.DateTimeField('date published', auto_now_add=True, auto_now=False)
+	def __str__(self):
+		return self.title
